d339c98e5eb036e00a91f736b8e61508e3e1deb4
EinarJohansson <ejrobb09@gmail.com>
2020-07-07 23:31:56 +0200

    Visar gymnasiegemensamma kurser.

--- database/create_user.js
+++ database/create_user.js
@@ -18,6 +18,68 @@ const create = (user) => {
           client.close()
           return resolve(response)
         }
+        // Lägg till gymnasiegemensamma kurser
+        user.kurser = {
+          pågående: [
+            {
+              kurs: 'Svenska 1',
+              poäng: 100,
+              typ: 'gymnasiegemensam',
+              betyg: ''
+            },
+            {
+              kurs: 'Svenska 2',
+              poäng: 100,
+              typ: 'gymnasiegemensam',
+              betyg: ''
+            },
+            {
+              kurs: 'Svenska 3',
+              poäng: 100,
+              typ: 'gymnasiegemensam',
+              betyg: ''
+            },
+            {
+              kurs: 'Engelska 5',
+              poäng: 100,
+              typ: 'gymnasiegemensam',
+              betyg: ''
+            },
+            {
+              kurs: 'Engelska 6',
+              poäng: 100,
+              typ: 'gymnasiegemensam',
+              betyg: ''
+            },
+            {
+              kurs: 'Matematik 1',
+              poäng: 100,
+              typ: 'gymnasiegemensam',
+              betyg: ''
+            },
+            {
+              kurs: 'Historia 1',
+              poäng: 50,
+              typ: 'gymnasiegemensam',
+              betyg: ''
+            },
+            {
+              kurs: 'Samhällskunskap 1',
+              poäng: 100,
+              typ: 'gymnasiegemensam',
+              betyg: ''
+            },
+            {
+              kurs: 'Religionskunskap',
+              poäng: 50,
+              typ: 'gymnasiegemensam',
+              betyg: ''
+            }
+          ],
+          kommande: [],
+          avslutade: []
+        }
+
         users.insertOne(user, (e, res) => {
           if (e) return reject(e)
           resolve(res.ops)
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,17 @@
 const router = require('express').Router()
 
 // Kolla om vi är authade eller inte
-const authCheck = (req, res, next) => { 
+const authCheck = (req, res, next) => {
     if (req.user) {
-        const data = {'namn': req.user.name, 'bild': req.user.picture, 'program': req.user.program, 'inriktning': req.user.inriktning} || {'namn': req.user.name, 'bild': req.user.picture}
+        const data =
+            {
+                'namn': req.user.name,
+                'bild': req.user.picture,
+                'program': req.user.program,
+                'inriktning': req.user.inriktning,
+                'kurser': req.user.kurser
+            } || { 'namn': req.user.name, 'bild': req.user.picture, 'kurser': req.user.kurser}
+
         res.render('pages/profil', data)
     }
     else next()
