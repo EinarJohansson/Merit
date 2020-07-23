5f5ae53b1c4d8c33775a882e30c837412c530e47
EinarJohansson <ejrobb09@gmail.com>
2020-07-23 22:32:15 +0200

    Ändrade schemat. Avänder inte ejs för table längre.

--- database/create_user.js
+++ database/create_user.js
@@ -15,86 +15,78 @@ const create = (user) => {
 
       users.find(user).toArray((err, response) => {
         if (err) return reject(err)
-        
+
         if (response.length > 0) {
           client.close()
           return resolve('Användaren fanns redan!')
         }
         // Lägg till Gymnasiegemensammama kurser
-        user.kurser = [
-          {
-            kurs: 'Svenska 1',
+        user.kurser =
+        {
+          'Svenska 1': {
             poäng: 100,
             typ: 'Gymnasiegemensamma',
             betyg: '',
             merit: Double(0.0),
             status: 'pågående'
           },
-          {
-            kurs: 'Svenska 2',
+          'Svenska 2': {
             poäng: 100,
             typ: 'Gymnasiegemensamma',
             betyg: '',
             merit: Double(0.0),
             status: 'pågående'
           },
-          {
-            kurs: 'Svenska 3',
+          'Svenska 3': {
             poäng: 100,
             typ: 'Gymnasiegemensamma',
             betyg: '',
             merit: Double(0.0),
             status: 'pågående'
           },
-          {
-            kurs: 'Engelska 5',
+          'Engelska 5': {
             poäng: 100,
             typ: 'Gymnasiegemensamma',
             betyg: '',
             merit: Double(0.0),
             status: 'pågående'
           },
-          {
-            kurs: 'Engelska 6',
+          'Engelska 6': {
             poäng: 100,
             typ: 'Gymnasiegemensamma',
             betyg: '',
             merit: Double(0.0),
             status: 'pågående'
           },
-          {
-            kurs: 'Matematik 1',
+          'Matematik 1': {
             poäng: 100,
             typ: 'Gymnasiegemensamma',
             betyg: '',
             merit: Double(0.0),
             status: 'pågående'
           },
-          {
-            kurs: 'Historia 1',
+          'Historia 1': {
             poäng: 50,
             typ: 'Gymnasiegemensamma',
             betyg: '',
             merit: Double(0.0),
             status: 'pågående'
           },
-          {
-            kurs: 'Samhällskunskap 1',
+          'Samhällskunskap 1': {
             poäng: 100,
             typ: 'Gymnasiegemensamma',
             betyg: '',
             merit: Double(0.0),
             status: 'pågående'
           },
-          {
-            kurs: 'Religionskunskap',
+          'Religionskunskap': {
             poäng: 50,
             typ: 'Gymnasiegemensamma',
             betyg: '',
             merit: Double(0.0),
             status: 'pågående'
           }
-        ]
+        }
 
         users.insertOne(user, (e, res) => {
           if (e) return reject(e)
